99fa7daf86debe7f7485aaa574e9222b20b05b8d
Greg's macbook pro <gas222@cornell.edu>
2017-03-08 14:19:12 -0500

    Add image picker.

--- DetailScreen.js
+++ DetailScreen.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
-import { Image, ScrollView, Text, View } from 'react-native';
+import { Image, ScrollView, Text, TouchableHighlight, View } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
 export default class DetailScreen extends Component {
 
   constructor(props) {
      super(props);
      this.state = {
-       text: ""
+       text: "",
+       avatarSource: null
      };
+   }
+
+   _handleImagePickerPress() {
+
+     const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+
+      if (response.didCancel) {
+        console.warn('User cancelled photo picker');
+      }
+      else if (response.error) {
+        console.warn('ImagePicker Error: ', response.error);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
    }
 
   componentDidMount() {
@@ -24,6 +55,9 @@ export default class DetailScreen extends Component {
   }
 
   render() {
+    const { avatarSource, text, showImage } = this.state;
+    const { imageUrl, title } = this.props;
+
     return (
       <View style={{flex: 1,
         flexDirection: 'column',
@@ -39,13 +73,28 @@ export default class DetailScreen extends Component {
               marginTop: 10,
               alignSelf: 'center'
             }}
-            source={{uri: this.props.imageUrl}}
+            source={{uri: imageUrl}}
           />
+        { avatarSource && <Image
+              style={{
+                width: 370,
+                height: 289,
+                marginTop: 10,
+                alignSelf: 'center'
+              }}
+              source={ avatarSource }
+            />
+        }
+        <TouchableHighlight onPress={() => this._handleImagePickerPress()} >
+          <Text style={{backgroundColor: 'red', fontWeight: '600', textAlign: 'center', fontFamily: 'helvetica' , fontSize: 17, flex: 0.7, marginTop: 5  }}>
+                Add your avatar!
+          </Text>
+        </TouchableHighlight>
         <Text style={{fontWeight: '600', textAlign: 'center', fontFamily: 'helvetica' , fontSize: 17, flex: 0.7, marginTop: 5  }}>
-              {this.props.title}
+              {title}
         </Text>
         <Text style={{ fontWeight: '400',  fontFamily: 'helvetica' , fontSize: 15, marginTop: 10 }}>
-                {this.state.text}
+                {text}
         </Text>
       </ScrollView>
       </View>
