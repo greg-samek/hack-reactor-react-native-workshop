89eb3837b5893a6dcf4079e7eda7ffaa6a9a3233
Greg Samek <gas222@cornell.edu>
2017-07-24 12:33:11 -0400

    Fix issue with step-2.

--- PopularListView.js
+++ PopularListView.js
@@ -22,7 +22,7 @@ export default class PopularListView extends Component {
        // Instantiate a new ListView dataSource.
        // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
        // Set dataSource using cloneWithRows.
-       //this.setState({dataSource: ds.cloneWithRows, loading: false});
+       //this.setState({dataSource: ds.cloneWithRows(json), loading: false});
      })
      .catch((error) => {
        console.warn(error);
